c98f48bec756ef166caa5065c9d1d9ddb843848b
Erik Flores <erik.flores@colorado.edu>
2019-05-03 16:48:55 -0600

    js-clock

---
+++ css-js-clock/app.js
@@ -0,0 +1,33 @@
+    const secondHand = document.querySelector('.second-hand');
+    const minuteHand = document.querySelector('.min-hand');
+    const hourHand = document.querySelector('.hour-hand');
+
+    function setDate() {
+
+    const now = new Date();
+
+    // get seconds
+    const seconds = now.getSeconds();
+    const secondsDegrees = ((seconds / 60) * 360) + 90;
+    secondHand.style.transform = `rotate(${secondsDegrees}deg)`;
+
+    // get minutes
+
+    const minutes = now.getMinutes();
+    const minutesDegrees = ((minutes / 60) * 360) + 90;
+    minuteHand.style.transform = `rotate(${minutesDegrees}deg)`;
+
+    // get hour
+    const hour = now.getHour();
+    const hourDegrees = ((hour / 12) * 360) + 90;
+    hourHand.style.transform = `rotate(${hourDegrees}deg)`;
+
+    // const date = new Date()
+    // const nowDate = date.toDateString()
+    // const time = date.toLocaleTimeString()
+    // document.getElementById("time").innerHtml = `${nowDate} ${time}`
+
+  }
+  setInterval(setDate, 1000);
+
+  setDate()
